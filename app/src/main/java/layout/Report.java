3d121239147fb8e18bb92d4793e845e8ff8a76ab
Huni <zxca1520@gmail.com>
2017-10-15 17:40:30 +0900

    Add NavigationBar

--- app/src/main/java/layout/Report.java
+++ app/src/main/java/layout/Report.java
@@ -1,13 +1,11 @@
 package layout;
 
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +14,6 @@ import android.widget.Toast;
 
 import com.hellopet.sangji.hellopet.R;
 
-import adapter.HelloPagerAdapter;
 import adapter.ReportPagerAdapter;
 
 /**
