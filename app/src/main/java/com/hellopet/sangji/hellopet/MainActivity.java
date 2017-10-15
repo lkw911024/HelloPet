bd97c4ed8197fc114266e8c42bea94db27403a90
Huni <zxca1520@gmail.com>
2017-10-15 18:05:22 +0900

    Update NavigationBar

--- app/src/main/java/com/hellopet/sangji/hellopet/MainActivity.java
+++ app/src/main/java/com/hellopet/sangji/hellopet/MainActivity.java
@@ -82,10 +82,37 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        // 나중에 스위치 케이스로 바꿀것
-        Log.i("TEST","@@@@@@@@@@@@@@@@@@@@@@@@@@@@id = " + id);
+        switch(id)
+        {
+            // MY MENU
+            case R.id.nav_profile:
+                Log.i("TEST","프로필 눌렀다");
+                break;
+            case R.id.nav_myPet:
+                Log.i("TEST","마이펫 눌렀다");
+                break;
+            case R.id.nav_my_InterestPet:
+                Log.i("TEST","나의 관심동물 눌렀다");
+                break;
+            case R.id.nav_registrationPet:
+                Log.i("TEST","동물등록 조회 눌렀다");
+                break;
+            case R.id.nav_NotificationArea:
+                Log.i("TEST","알림 지역 설정 눌렀다");
+                break;
 
-        // MY MENU
+            // INFORMATION
+            case R.id.nav_notice:
+                Log.i("TEST","공지사항 눌렀다");
+                break;
+            case R.id.nav_question:
+                Log.i("TEST","문의하기 눌렀다");
+                break;
+            case R.id.nav_often_question:
+                Log.i("TEST","자주하는 질문 눌렀다");
+                break;
+        }
+        /*
         if (id == R.id.nav_profile) {
             Log.i("TEST","프로필 눌렀다");
         } else if (id == R.id.nav_myPet) {
@@ -107,6 +134,7 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
         else if (id == R.id.nav_often_question) {
             Log.i("TEST","자주하는 질문 눌렀다");
         }
+        */
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
